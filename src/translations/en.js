56a8b8ced995cf8dd0bdf6f0baef0673198004c5
Renox1527 <reny.alvarezxd@gmail.com>
2026-01-23 21:04:47 -0400

    Add bilingual structure to portfolio

---
+++ src/translations/en.js
@@ -0,0 +1,12 @@
+export default {
+  nav: {
+    home: "Home",
+    about: "About",
+    projects: "Projects",
+    contact: "Contact",
+  },
+  home: {
+    title: "Hi, I'm Reny",
+    subtitle: "Front-End Developer",
+  },
+};
---
+++ src/translations/es.js
@@ -0,0 +1,12 @@
+export default {
+  nav: {
+    home: "Inicio",
+    about: "Sobre mí",
+    projects: "Proyectos",
+    contact: "Contacto",
+  },
+  home: {
+    title: "Hola, soy Reny",
+    subtitle: "Desarrollador Front-End",
+  },
+};
